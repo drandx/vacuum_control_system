2efa2de5cf59b2a917b8f90152bbd1690063d8d8
drandx <garjuanpablo@gmail.com>
2014-10-13 15:02:28 -0500

    Added getters and setters to the Simulator Model.

--- src/simulator/model/SimulatorDomainModel.java
+++ src/simulator/model/SimulatorDomainModel.java
@@ -6,12 +6,53 @@ public class SimulatorDomainModel {
 	public class Home
 	{
 		LinkedList<FloorLevel> Floors;
+		
+		public Home() {
+			super();
+			Floors = new LinkedList<SimulatorDomainModel.FloorLevel>();
+		}
+
+		public Home(LinkedList<FloorLevel> floors) {
+			super();
+			Floors = floors;
+		}
+
+		public LinkedList<FloorLevel> getFloors() {
+			return Floors;
+		}
+
+		public void setFloors(LinkedList<FloorLevel> floors) {
+			Floors = floors;
+		}
+		
 	}
 	
 	public class FloorLevel
 	{
 		int floor;
 		LinkedList<Cell> Cells;
+		
+		public FloorLevel(int floor, LinkedList<Cell> cells) {
+			super();
+			this.floor = floor;
+			Cells = cells;
+		}
+		public FloorLevel() {
+			super();
+		}
+		public int getFloor() {
+			return floor;
+		}
+		public void setFloor(int floor) {
+			this.floor = floor;
+		}
+		public LinkedList<Cell> getCells() {
+			return Cells;
+		}
+		public void setCells(LinkedList<Cell> cells) {
+			Cells = cells;
+		}
+		
 	}
 	
 	public class Cell
@@ -22,6 +63,54 @@ public class SimulatorDomainModel {
 		String ps;
 		String ds;
 		String cs;
+		
+		public Cell(String xs, String ys, String ss, String ps, String ds,
+				String cs) {
+			super();
+			this.xs = xs;
+			this.ys = ys;
+			this.ss = ss;
+			this.ps = ps;
+			this.ds = ds;
+			this.cs = cs;
+		}
+		public String getXs() {
+			return xs;
+		}
+		public void setXs(String xs) {
+			this.xs = xs;
+		}
+		public String getYs() {
+			return ys;
+		}
+		public void setYs(String ys) {
+			this.ys = ys;
+		}
+		public String getSs() {
+			return ss;
+		}
+		public void setSs(String ss) {
+			this.ss = ss;
+		}
+		public String getPs() {
+			return ps;
+		}
+		public void setPs(String ps) {
+			this.ps = ps;
+		}
+		public String getDs() {
+			return ds;
+		}
+		public void setDs(String ds) {
+			this.ds = ds;
+		}
+		public String getCs() {
+			return cs;
+		}
+		public void setCs(String cs) {
+			this.cs = cs;
+		}
+		
 	}
 	
 }
