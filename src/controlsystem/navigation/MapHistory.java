928eff56d7f032c78b78552f3901979836f8a1a6
Jase Kurasz <jase.kurasz@gmail.com>
2014-10-18 17:41:22 -0500

    added to the testing class of the navigation package

--- src/controlsystem/navigation/MapHistory.java
+++ src/controlsystem/navigation/MapHistory.java
@@ -15,7 +15,8 @@ public class MapHistory {
 	public void addHistory(coordPair history) {
 		historyMap.add(history);
 		
-		//Distance to charing station is just the total sum of the X and Y coordinates its currently on.
+		//Closest Distance to charing station is just the total sum of the X and Y coordinates its currently on.
+		//This will be the min battery it needs to get to charging station
 		distToHome = Math.abs(history.getX()) + Math.abs(history.getY());
 		
 	}
--- src/controlsystem/navigation/test.java
+++ src/controlsystem/navigation/test.java
@@ -1,10 +1,28 @@
 package controlsystem.navigation;
 
+import simulator.model.Cell;
+import simulator.model.HomeModel;
+
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		HomeModel.Load();
+		MapHistory map = new MapHistory();
+		for(int i=0; i < HomeModel.loadedHome.getFloors().size(); i++)
+		{
+			for(int j = 0; j < HomeModel.loadedHome.getFloors().get(i).getCells().size(); j++)
+			{
+				Cell currentCell = HomeModel.loadedHome.getFloors().get(i).getCells().get(j);
+				
+				coordPair pair = new coordPair();
+				pair.setX(currentCell.getXs());
+				pair.setY(currentCell.getYs());
+				
+				map.addHistory(pair);
+				System.out.println(map.getDistToHome());
+			}
+		}
 	}
+
 
 }
